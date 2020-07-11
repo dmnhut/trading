a0f543009796c3b29617a78ea1411064e081bf1d
dmnhut <doanminhnhut.vn@gmail.com>
2020-07-12 03:13:14 +0700

    format import

--- public/app/index.js
+++ public/app/index.js
@@ -1,39 +1,15 @@
-import {
-    PageCommon
-} from './container/common';
-import {
-    DetailShippers
-} from './detail-shippers';
-import {
-    MapIndex
-} from './map';
-import {
-    MapLocation
-} from './map/location';
-import {
-    OrdersCreate
-} from './orders/create';
-import {
-    OrdersEdit
-} from './orders/edit';
-import {
-    Portal
-} from './portal';
-import {
-    Prices
-} from './prices';
-import {
-    Units
-} from './units';
-import {
-    UsersCreate
-} from './users/create';
-import {
-    UsersEdit
-} from './users/edit';
-import {
-    UsersInfo
-} from './users/info';
+import { PageCommon } from './container/common';
+import { DetailShippers } from './detail-shippers';
+import { MapIndex } from './map';
+import { MapLocation } from './map/location';
+import { OrdersCreate } from './orders/create';
+import { OrdersEdit } from './orders/edit';
+import { Portal } from './portal';
+import { Prices } from './prices';
+import { Units } from './units';
+import { UsersCreate } from './users/create';
+import { UsersEdit } from './users/edit';
+import { UsersInfo } from './users/info';
 
 $(document).ready(() => {
 
